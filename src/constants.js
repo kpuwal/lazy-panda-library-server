a968c23b0b9f272761c1c9b8c1d56b53371764c1
kpuwal <kasia@rebood.com>
2023-10-25 02:24:24 +0200

    adds saving and updating messages

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+const savedToPandaLibraryMessages = [
+  "🐼 A new adventure awaits! Your book is safely stored in the Panda Library. 📚",
+  "🐼 Your book is now a part of our bamboo-filled world! Find it in the Panda Library. 📖🌿",
+  "🐼 Another book finds a home in the Panda Library. Your collection is growing! 📚🐾",
+  "🐼 It's a panda party! Your book is in the Panda Library. Let's keep the stories going! 🎉📚",
+  "🐼 Your book now resides in the Panda Library, waiting to be explored. Enjoy the reading journey! 🐼📖",
+];
+
+const updatedPandaLibraryMessages = [
+  "🐼 A book has been polished and updated in the Panda Library. It's better than ever! 📚✨",
+  "🐼 Your book just got a makeover! Check out the refreshed version in the Panda Library. 📖💅",
+  "🐼 Updates are in! Your book now shines brighter in the Panda Library. Enjoy the enhanced reading experience! 📚💫",
+  "🐼 It's like magic! Your book has been updated and is ready to surprise you in the Panda Library. ✨📖",
+  "🐼 Exciting news! Your book has been spruced up in the Panda Library. Rediscover it today! 🎉📚",
+];
+
+const errorUpdatingSpreadsheetMessages = [
+  "Oops, looks like our tech-savvy pandas had a keyboard mishap! 🐼💻 Don't worry; they're fixing it. Please try updating again in a moment.",
+  "It appears our pandas had a dance party on the keyboard, and things got a bit jumbled! 🐼🕺💃 We're unscrambling it. Please reattempt the update shortly.",
+  "Panda programmers hit a little snag in the matrix. 🐼🤖 They're debugging it now. Try updating again when the code is clean and error-free.",
+  "Whoops, a panda bear emoji invaded our spreadsheet! 🐼📉 We're on a panda hunt to remove it. Please update again once the pandas have returned to their bamboo desks.",
+  "Spreadsheets are tricky—our pandas might've done some panda-typing! 🐼📝 We're sorting through it. Please update again when it's all pandas and no paws."
+];
+
+
+const errorSavingToLibraryMessages = [
+  "Oops, looks like our library pandas had a tea break and the book got misplaced! 🐼🍵 Don't worry; they're searching for it. Please try saving again in a moment.",
+  "Uh-oh! 🐼😅 The book's adventurous spirit led it astray in our library. Our panda adventurers are on the quest to retrieve it. Try saving again shortly.",
+  "Weird things are happening in the library! 📚🌀 The book decided to explore uncharted shelves. Our pandas are on a mission to bring it back. Please save again later.",
+  "Safari time! 🐼🌿 Your book turned into a panda explorer and got lost in the library jungle. Our guides are tracking it down. Try saving again soon.",
+  "Hmmm... it seems the book decided to take a detour on its way to the library. Our library pandas are experts at treasure hunts. They'll find it. Please try saving later.",
+];
+
+module.exports = {
+  savedToPandaLibraryMessages,
+  updatedPandaLibraryMessages,
+  errorUpdatingSpreadsheetMessages,
+  errorSavingToLibraryMessages
+}
