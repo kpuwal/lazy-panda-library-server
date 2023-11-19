97e430c6594cad909402a719e4ceeb261a911fd4
kpuwal <kasia@rebood.com>
2023-11-19 17:13:33 +0100

    adds update picker route and orc tesseract library

--- router.js
+++ router.js
@@ -1,13 +1,21 @@
 const Router = require('express-promise-router');
 const { googleRequest } = require('./src/google');
 const auth = require('./src/auth');
+const multer = require('multer');
+
 const {
   writeLibrary,
   readLibrary,
   updateLibrary,
   readPicker,
+  updatePicker,
   filterLibrary
 } = require('./src/sheets');
+const { processPicture } = require('./src/tesseract'); 
+
+// Configure multer to handle file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const router = new Router();
 
@@ -17,5 +25,7 @@ router.get('/api/library', auth, readLibrary);
 router.get('/api/picker', auth, readPicker);
 router.post('/api/update-library', auth, updateLibrary);
 router.post('/api/filter-library', auth, filterLibrary);
+router.post('/api/update-picker', auth, updatePicker);
+router.post('/api/process-picture', auth, upload.single("image"), processPicture);
 
 module.exports = router;
--- src/sheets.js
+++ src/sheets.js
@@ -152,6 +152,104 @@ const readPicker = async (_req, res) => {
   }
 }
 
+// const updatePicker = async (_req, res) => {
+//   try {
+//     const { columnName, data } = _req.body;
+
+//     const { sheets } = await authentication();
+//     const sheetValues = await sheets.spreadsheets.values.get({
+//       spreadsheetId: PICKER_SHEET_ID,
+//       range: 'Output',
+//       majorDimension: 'ROWS',
+//     });
+
+//     const headers = sheetValues.data.values[0] || [];
+//     let targetColumnIndex = headers.findIndex(title => !title); // Find first empty column
+
+//     if (targetColumnIndex === -1) {
+//       targetColumnIndex = headers.length; // If no empty column is found, append a new column
+//     }
+
+//     // Update the title in the first row
+//     await sheets.spreadsheets.values.update({
+//       spreadsheetId: PICKER_SHEET_ID,
+//       range: `Output!${String.fromCharCode(targetColumnIndex + 65)}1`,
+//       valueInputOption: 'RAW',
+//       resource: {
+//         values: [[columnName]],
+//       },
+//     });
+
+//     // Update the target cell in the specified column
+//     await sheets.spreadsheets.values.update({
+//       spreadsheetId: PICKER_SHEET_ID,
+//       range: `Output!${String.fromCharCode(targetColumnIndex + 65)}2`,
+//       valueInputOption: 'RAW',
+//       resource: {
+//         values: data.map(value => [value]),
+//       },
+//     });
+
+//     return res.json({ msg: 'Data appended successfully' });
+//   } catch (err) {
+//     console.log('ERROR UPDATING PICKER SHEET: ', err);
+//     res.status(500).send(err);
+//   }
+// };
+
+const updatePicker = async (_req, res) => {
+  try {
+    const { columnName, data } = _req.body;
+
+    const { sheets } = await authentication();
+    const sheetValues = await sheets.spreadsheets.values.get({
+      spreadsheetId: PICKER_SHEET_ID,
+      range: 'Output',
+      majorDimension: 'ROWS',
+    });
+
+    const headers = sheetValues.data.values[0] || [];
+    let targetColumnIndex = headers.indexOf(columnName);
+
+    if (targetColumnIndex === -1) {
+      // Column doesn't exist, create a new column
+      targetColumnIndex = headers.length;
+
+      // Update the title in the first row
+      await sheets.spreadsheets.values.update({
+        spreadsheetId: PICKER_SHEET_ID,
+        range: `Output!${String.fromCharCode(targetColumnIndex + 65)}1`,
+        valueInputOption: 'RAW',
+        resource: {
+          values: [[columnName]],
+        },
+      });
+    }
+
+    // Clear the entire column starting from row 2
+    await sheets.spreadsheets.values.clear({
+      spreadsheetId: PICKER_SHEET_ID,
+      range: `Output!${String.fromCharCode(targetColumnIndex + 65)}2:${String.fromCharCode(targetColumnIndex + 65)}`,
+    });
+
+    // Update the target column with new data starting from row 2
+    await sheets.spreadsheets.values.update({
+      spreadsheetId: PICKER_SHEET_ID,
+      range: `Output!${String.fromCharCode(targetColumnIndex + 65)}2`,
+      valueInputOption: 'RAW',
+      resource: {
+        values: data.map(value => [value]),
+      },
+    });
+
+    return res.json({ msg: 'Column updated/appended successfully' });
+  } catch (err) {
+    console.log('ERROR UPDATING PICKER SHEET: ', err);
+    res.status(500).send(err);
+  }
+};
+
+
 const authentication = async () => {
   const auth = new google.auth.GoogleAuth({
     keyFile: './google-credentials.json',
@@ -171,5 +269,6 @@ module.exports = {
   readLibrary,
   updateLibrary,
   readPicker,
-  filterLibrary
+  filterLibrary,
+  updatePicker
 }
